565ccf0b28da78907e3be0e607a493af43c3e1ca
gharynsl123 <persolna1243@gmail.com>
2023-02-24 07:58:00 +0700

    update

---
+++ resources/views/admin/menu.blade.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="index.html" class="nav-link active"><i class="bx bx-home"></i> <span>Dashboard</span></a></li>
+    <li><a href="create-user.html" class="nav-link"><i class="bx bx-user"></i> <span>Create Staff User</span></a></li>
+    <li><a href="absensi-perbulan.html" class="nav-link"><i class="bx bx-book"></i> <span>Data Absensi Perbulan</span></a></li>
+    <li><a href="#portfolio" class="nav-link"><i class="bx bx-server"></i> <span>Log Out</span></a></li>
+</ul>
