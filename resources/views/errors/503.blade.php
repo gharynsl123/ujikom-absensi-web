a0f8c4c10f2680673f4f855407b8a4e186108d3c
gharynsl123 <persolna1243@gmail.com>
2023-05-15 10:23:44 +0700

    error view

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Server Maintenance</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container d-flex flex-column vh-100 justify-content-center align-items-center">
+        <img src="{{ url('storage/icon-web.png') }}" alt="Icon Sekolah" class="img-fluid" style="max-width: 200px; max-height: 200px;">
+        <p class="text-lowercase fs-3 fw-bold m-0">Sebentarya Serve Sedang Down</p>
+        <p class="text-secondary">Coba Kembali Beberapa saat lagi</p>
+    </div>
+</body>
+</html>
